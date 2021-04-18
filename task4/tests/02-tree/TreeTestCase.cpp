6a442e334f576e695f93b5ccd60dcc0b67314e6e
Nikita Mastinen <nikmastinen@gmail.com>
2021-04-18 16:56:12 +0300

    Tested tree 100%

--- task4/tests/02-tree/TreeTestCase.cpp
+++ task4/tests/02-tree/TreeTestCase.cpp
@@ -10,13 +10,17 @@ class Fixture: public::testing::Test {
 public:
   std::string test_path;
   std::string path_to_test_file;
+  std::string path_to_empty_folder;
+  std::string current_path;
 
   void SetUp() final {
-    auto current_path = boost::filesystem::current_path().string();
+    current_path = boost::filesystem::current_path().string();
     test_path = current_path + "/testdir";
     boost::filesystem::create_directory(test_path);
-    boost::filesystem::create_directory(test_path + "/testdir2");
+    path_to_empty_folder = test_path + "/testdir2";
+    boost::filesystem::create_directory(path_to_empty_folder);
     path_to_test_file = test_path + "/test.txt";
+
     std::ofstream file(path_to_test_file);
     file.close();
   }
@@ -32,7 +36,7 @@ TEST_F(Fixture, TestGetTree)
   ASSERT_THROW(GetTree(Fixture::path_to_test_file, true), std::invalid_argument);
   ASSERT_EQ(GetTree(Fixture::test_path, true), GetTree(Fixture::test_path, true));
   ASSERT_TRUE(GetTree(Fixture::test_path, false) == GetTree(Fixture::test_path, false));
-  ASSERT_NO_THROW(FilterEmptyNodes(GetTree(Fixture::test_path, true), Fixture::test_path));
+  ASSERT_NO_THROW(FilterEmptyNodes(GetTree(Fixture::current_path, false), Fixture::current_path));
 }
 
 
